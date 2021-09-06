e36d7e474d46db8a2a81e8dce6626959d70d4ac6
jessicazu <jessicazu926@gmail.com>
2021-09-06 17:15:36 +0900

    tmp

---
+++ src/features/userSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from '../app/store'
+
+const initialState = {
+  user: {
+    uid: '',
+    photoUrl: '',
+    displayName: '',
+  },
+}
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload
+    },
+    logout: (state) => {
+      state.user = initialState.user
+    },
+  },
+})
+
+export const { login, logout } = userSlice.actions
+
+export const selectUser = (state: RootState) => state.user.user
+
+export default userSlice.reducer
